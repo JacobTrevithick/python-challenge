15c87317cdd24ab95716a9790d1efd67382bfaab
Jacob Trevithick <jaketrevithick@Jakes-MBP.attlocal.net>
2021-08-23 12:15:39 -0700

    PyBank main.py finalized with comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,28 +1,36 @@
+#importing os and csv libraries to handle files
 import os
 import csv
-# import statistics as stat
+
 
 def average(num_list):
+    '''
+    Gets average of all values in a list
+    '''
     return sum(num_list) / len(num_list)
 
 def get_changes(num_list):
     '''
     Adapted function for finding the next value in a list using enumerate method.
 
+    Take the difference between the current and previous value in list, and append that to a new 'change_list'. Does not calculate for very first number in the list
+
     Code adapted from Kite online coding resource:
     https://www.kite.com/python/answers/how-to-access-previous-and-next-values-when-looping-through-a-list-in-python#:~:text=Use%20enumerate()%20to%20access,list%5Bindex%2D1%5D%20.
     '''
     change_list = []
     for index, num in enumerate(num_list):
-        if index -1 >= 0:
+        if index - 1 >= 0:
             change_list.append(num_list[index] - num_list[index - 1])
     return change_list
 
-
+#creating budget_data_csv file object
 budget_data_csv = os.path.join("Resources", 'budget_data.csv')
 
+#open budget file object as csv file
 with open(budget_data_csv, 'r') as csv_file:
     
+    #create csv reader object to iterate through months
     budget_data = csv.reader(csv_file, delimiter = ',') 
 
     total_months = 0
@@ -30,20 +38,30 @@ with open(budget_data_csv, 'r') as csv_file:
     profit_list = []
     month_list = []
 
+    #skip header row
     csv_header = next(budget_data, None)
 
+    #iterate through each month
     for row in budget_data:
         
+        #count total months and sum total profits/losses
         total_months += 1
         net_profit += int(row[1])
 
+        #populating list of months and list of profit values to use later
         month_list.append(row[0])
         profit_list.append(int(row[1]))
 
+#calc profit changes from month-to-month using get_changes function
+#average those changes and round to 2 decimal places
 profit_changes = get_changes(profit_list)
 avg_change = round(average(profit_changes), 2)
 
-#Terminal printing
+'''
+Terminal Printing:
+Month corresponding to greatest increase and decrease are found by retrieving the index of the max/min profit change and corresponding that to the month_list. 
+Note: month_list index must be +1 because profit change not calc'd for first month.
+'''
 print("Financial Analysis")
 print("-" * 30)
 print(f"Total Months: {total_months}")
@@ -52,10 +70,14 @@ print(f"Average Change: ${avg_change}")
 print(f"Greatest Increase in Profits: {month_list[profit_changes.index(max(profit_changes)) + 1]} (${max(profit_changes)})")
 print(f"Greatest Decrease in Profits: {month_list[profit_changes.index(min(profit_changes)) + 1]} (${min(profit_changes)})")
 
+
+#creating financial analysis text file 
 financial_analysis_file = os.path.join("analysis", 'Financial_Analysis.txt')
 
 with open(financial_analysis_file, 'w') as txt_file:
     
+    #Write financial report to text file.
+
     txt_file.write("Financial Analysis\n")
     txt_file.write("---------------------------------------\n")
     txt_file.write(f"Total Months: {total_months}\n")
